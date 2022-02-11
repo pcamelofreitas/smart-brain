e110e91cac41cd0a13d81a6ca489ec59068b10b1
Pedro Freitas <pcamelofreitas@gmail.com>
2022-02-11 11:35:34 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -88,7 +88,6 @@ class App extends Component {
                     })
             .then(response => response.json())
             .then(response => {
-                console.log(response);
                 if(response){
                     fetch('http://localhost:3000/image',{
                         method: 'put',
